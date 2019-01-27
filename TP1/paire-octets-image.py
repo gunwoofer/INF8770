29782ad8a7fb7e100e93b36db9760f4a4d0d232d
Benjamin <benjamin150594@hotmail.fr>
2019-01-27 16:13:42 -0500

    avance

--- TP1/paire-octets-image.py
+++ TP1/paire-octets-image.py
@@ -1,66 +1,91 @@
+ # -*-coding:Latin-1 -* 
+
 import numpy as np
 import matplotlib.pyplot as py
 import time
+import regex as re
+
 
 print ("Codage paire octets image..")
 
 def rgb2gray(rgb):
     return np.dot(rgb[:,:], [0.299, 0.587, 0.114])
 
+def convertNumber(array):
+    for i in range (len(array)):
+        if len(array[i]) == 1:
+            array[i] = '00' + array[i]
+        elif len(array[i]) == 2:
+            array[i] = '0' + array[i]
+
 fig1 = py.figure(figsize = (10,10))
-imagelue = py.imread('RGB.jpg')
+imagelue = py.imread('degrade.jpg')
 image=imagelue.astype('float')
 image=rgb2gray(image)
-imageout=image.astype('uint8').flatten().astype("character")
+
+imageout=image.astype('uint8').flatten().astype("str")
+convertNumber(imageout)
+
+Message = ''.join(imageout)
 
 # py.imshow(imageout,cmap = py.get_cmap('gray'))
 # py.show()
 
+#Message = "001100011110001010101011110001101010111111111111110000000000000000110101010100011101010101"
+#Message =""
+#for i in range(33020):
+ #   if i < 33020 /2:
+  #      Message += '0'
+   # else:
+    #    Message += '1'
+LUToctetsdispo = [True] * 0xffff
+dictsymb =[Message[0]]
+LUToctetsdispo[ord(Message[0])] = False
 
-LUToctetsdispo = [True] * 256
-dictsymb =[imageout[0]]
-LUToctetsdispo[imageout[0].astype('uint8')] = False
 nbsymboles = 1
-for i in range(1,len(imageout)):
-    if imageout[i] not in dictsymb:
-        dictsymb += [imageout[i]]
-        LUToctetsdispo[imageout[i].astype('uint8')] = False  #Octet utilisé
+start_time = time.time()
+
+for i in range(1,len(Message)):
+    if Message[i] not in dictsymb:
+        dictsymb += [Message[i]]
+        LUToctetsdispo[ord(Message[i])] = False  #Octet utilisé
         nbsymboles += 1
         
-longueurOriginale = np.ceil(np.log2(nbsymboles))*len(imageout)
+longueurOriginale = np.ceil(np.log2(nbsymboles))*len(Message)
 
 dictsymb = []  #Dictionnaire des substitutions
-debut = imageout[0]  # Origine trouver un code de substitution. Et pour avoir des caractères imprimables...
+debut = ord(Message[0])  # Origine trouver un code de substitution. Et pour avoir des caractères imprimables...
 
 remplacementpossible = True
 while remplacementpossible == True:
     #Recherche des paires
     paires = []
-    for i in range(0,len(imageout)-1):
-        temppaire = imageout[i]+imageout[i+1]
-        if not list(filter(lambda x: x[0] == temppaire, paires)): #Si la liste retournée par filter est vide.
-            paires += [[temppaire,len(re.findall(temppaire, imageout, overlapped = True))]]
+    for i in range(0,len(Message)-1):
+        symbole1, symbole2 = Message[i], Message[i+1]
+        temppaire = Message[i] + Message[i+1]
+        if not list(filter(lambda x: x[0] == temppaire, paires)): #Si la liste retournee par filter est vide.
+            longueur = Message.count(temppaire)
+            paires += [[temppaire, longueur]]
 
-    #Trouve la paire avec le plus de répétitions.
+    #Trouve la paire avec le plus de repetitions.
     paires = sorted(paires, key=lambda x: x[1], reverse = True)
 
     if paires[0][1] > 1:
         #Remplace la paire
         print(paires)
-        print("La paire ",paires[0][0], " est la plus fréquente avec ",paires[0][1], "répétitions")
-        #Cherche un octet non utilisé
-        while debut <256 and LUToctetsdispo[debut] == False:
+        print("La paire ",paires[0][0], " est la plus frequente avec ",paires[0][1], "repetitions")
+        #Cherche un octet non utilise
+        while debut <0xffff and LUToctetsdispo[debut] == False:
             debut += 1
-        if debut < 256:     
+        if debut < 0xffff:     
             #On substitut
-            imageout = imageout.replace(paires[0][0], chr(debut))
+            Message = Message.replace(paires[0][0],  unichr(debut))
             LUToctetsdispo[debut] = False
-            dictsymb += [[paires[0][0], chr(debut)]]
+            dictsymb += [[paires[0][0], unichr(debut)]]
         else:
-            print("Il n'y a plus d'octets disponible!") #Bien sûr, ce n'est pas exact car la recherche commence à imageout[0]
-        
-        print(imageout)
-        print(dictsymb)
+            print("Il n y a plus d octets disponible!")
+            remplacementpossible = False
+
     else:
         remplacementpossible = False
 
@@ -68,6 +93,6 @@ while remplacementpossible == True:
 temps_execution = time.time() - start_time
 print("Temps d execution : ", time.time() - start_time, " secondes")
 
-print("Longueur = {0}".format(np.ceil(np.log2(nbsymboles))*len(imageout)))
+print("Longueur = {0}".format(np.ceil(np.log2(nbsymboles))*len(Message)))
 print("Longueur originale = {0}".format(longueurOriginale))
 
--- TP1/predictif.py
+++ TP1/predictif.py
@@ -1,6 +1,9 @@
+ # -*-coding:Latin-1 -* 
+
 import numpy as np
 import matplotlib.pyplot as py
 import time
+from anytree import Node, RenderTree, PreOrderIter, AsciiStyle
 
 print ("Codage Predictif")
 
@@ -8,19 +11,32 @@ def rgb2gray(rgb):
     return np.dot(rgb[:,:], [0.299, 0.587, 0.114])
 
 fig1 = py.figure(figsize = (10,10))
-imagelue = py.imread('RGB.jpg')
+imagelue = py.imread('degrade.jpg')
 image=imagelue.astype('float')
 image=rgb2gray(image)
 imageout=image.astype('uint8')
 py.imshow(imageout,cmap = py.get_cmap('gray'))
-py.show()
+#py.show()
 
-hist, intervalles = np.histogram(imageout, bins=256)
-py.bar(intervalles[:-1], hist, width = 2)
-py.xlim(min(intervalles)-1, max(intervalles))
-py.show()
+#hist, intervalles = np.histogram(imageout, bins=256)
+#py.bar(intervalles[:-1], hist, width = 2)
+#py.xlim(min(intervalles)-1, max(intervalles))
+#py.show()
 
+#image = "001100011110001010101011110001101010111111111111110000000000000000110101010100011101010101"
 
+#image =""
+#for i in range(32761):
+    #if i < 32761 / 2:
+    #   image += '0'
+    #else:
+    #    image += '1'
+#image = list(image)
+#image = np.reshape(image,(-1,181))
+#image = image.astype('uint8')
+#imageout=image.astype('uint8')
+#py.imshow(imageout,cmap = py.get_cmap('gray'))
+#py.show()
 # Debut du Timer
 start_time = time.time()
 
@@ -49,16 +65,119 @@ print("Temps d execution : ", time.time() - start_time, " secondes")
 hist, intervalles = np.histogram(erreur, bins=100)
 py.bar(intervalles[:-1], hist, width = 2)
 py.xlim(min(intervalles)-1, max(intervalles))
-py.show()
+#py.show()
 
 fig2 = py.figure(figsize = (10,10))
 imageout=imagepred.astype('uint8')
 py.imshow(imageout, cmap = py.get_cmap('gray'))
-py.show()
+#py.show()
 
 
 fig3 = py.figure(figsize = (10,10))
 erreur=abs(erreur)*5
 imageout=erreur.astype('uint8')
 py.imshow(imageout, cmap = py.get_cmap('gray'))
-py.show()
+imageout = imageout.flatten().astype('str')
+
+
+Message = ''.join(imageout)
+
+ArbreSymb =[[Message[0], Message.count(Message[0]), Node(Message[0])]] 
+#dictionnaire obtenu à partir de l'arbre.
+dictionnaire = [[Message[0], '']]
+nbsymboles = 1
+
+#Recherche des feuilles de l'arbre
+
+for i in range(1,len(Message)):
+    if not list(filter(lambda x: x[0] == Message[i], ArbreSymb)):
+        ArbreSymb += [[Message[i], Message.count(Message[i]),Node(Message[i])]]
+        dictionnaire += [[Message[i], '']]
+        nbsymboles += 1
+        
+longueurOriginale = np.ceil(np.log2(nbsymboles))*len(Message)
+
+ArbreSymb = sorted(ArbreSymb, key=lambda x: x[1])
+#print(ArbreSymb)
+
+
+
+while len(ArbreSymb) > 1:
+    #Fusion des noeuds de poids plus faibles
+    symbfusionnes = ArbreSymb[0][0] + ArbreSymb[1][0] 
+    #Création d'un nouveau noeud
+    noeud = Node(symbfusionnes)
+    temp = [symbfusionnes, ArbreSymb[0][1] + ArbreSymb[1][1], noeud]
+    #Ajustement de l'arbre pour connecter le nouveau avec ses parents 
+    ArbreSymb[0][2].parent = noeud
+    ArbreSymb[1][2].parent = noeud
+    #Enlève les noeuds fusionnés de la liste de noeud à fusionner.
+    del ArbreSymb[0:2]
+    #Ajout du nouveau noeud à la liste et tri.
+    ArbreSymb += [temp]
+    #Pour affichage de l'arbre ou des sous-branches
+   # print('\nArbre actuel:\n\n')
+    for i in range(len(ArbreSymb)):
+        if len(ArbreSymb[i][0]) > 1:
+            #print(RenderTree(ArbreSymb[i][2], style=AsciiStyle()).by_attr())   
+            pass
+    ArbreSymb = sorted(ArbreSymb, key=lambda x: x[1])  
+    #print(ArbreSymb)
+
+
+ArbreCodes = Node('')
+noeud = ArbreCodes
+#print([node.name for node in PreOrderIter(ArbreSymb[0][2])])
+parcoursprefix = [node for node in PreOrderIter(ArbreSymb[0][2])]
+parcoursprefix = parcoursprefix[1:len(parcoursprefix)] #ignore la racine
+
+Prevdepth = 0 #pour suivre les mouvements en profondeur dans l'arbre
+for node in parcoursprefix:  #Liste des noeuds 
+    if Prevdepth < node.depth: #On va plus profond dans l'arbre, on met un 0
+        temp = Node(noeud.name + '0')
+        noeud.children = [temp]
+        if node.children: #On avance le "pointeur" noeud si le noeud ajouté a des enfants.
+            noeud = temp
+    elif Prevdepth == node.depth: #Même profondeur, autre feuille, on met un 1
+        temp = Node(noeud.name + '1')
+        noeud.children = [noeud.children[0], temp]  #Ajoute le deuxième enfant
+        if node.children: #On avance le "pointeur" noeud si le noeud ajouté a des enfants.
+            noeud = temp
+    else:
+        for i in range(Prevdepth-node.depth): #On prend une autre branche, donc on met un 1
+            noeud = noeud.parent #On remontre dans l'arbre pour prendre la prochaine branche non explorée.
+        temp = Node(noeud.name + '1')
+        noeud.children = [noeud.children[0], temp]
+        if node.children:
+            noeud = temp
+        
+    Prevdepth = node.depth    
+    
+#print('\nArbre des codes:\n\n',RenderTree(ArbreCodes, style=AsciiStyle()).by_attr())         
+#print('\nArbre des symboles:\n\n', RenderTree(ArbreSymb[0][2], style=AsciiStyle()).by_attr())
+
+ArbreSymbList = [node for node in PreOrderIter(ArbreSymb[0][2])]
+ArbreCodeList = [node for node in PreOrderIter(ArbreCodes)]
+
+for i in range(len(ArbreSymbList)):
+    if ArbreSymbList[i].is_leaf: #Génère des codes pour les feuilles seulement
+        temp = list(filter(lambda x: x[0] == ArbreSymbList[i].name, dictionnaire))
+        if temp:
+            indice = dictionnaire.index(temp[0])
+            dictionnaire[indice][1] = ArbreCodeList[i].name
+            
+#print(dictionnaire)
+
+
+MessageCode = []
+longueur = 0 
+for i in range(len(Message)):
+    substitution = list(filter(lambda x: x[0] == Message[i], dictionnaire))
+    MessageCode += [substitution[0][1]]
+    longueur += len(substitution[0][1])
+
+#print(MessageCode)
+print("Temps d execution : ", time.time() - start_time, " secondes")
+
+print("Longueur = {0}".format(longueur))
+print("Longueur originale = {0}".format(longueurOriginale))
