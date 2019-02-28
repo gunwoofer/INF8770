23e35b3e3b9575fec61f9626a14ac034f6291d53
Benjamin <benjamin150594@hotmail.fr>
2019-02-28 09:39:53 -0500

    etape 1 fini

--- TP2/main.py
+++ TP2/main.py
@@ -1,59 +1,73 @@
 import matplotlib.pyplot as py
 import numpy as np
 
+INDEX_Y = 0
+INDEX_CB = 1
+INDEX_CR = 2
+
+INDEX_R = 0
+INDEX_G = 1
+INDEX_B = 2
+
 print ("TP2 INF8770..")
 
-image = py.imread("image.jpeg")
+image = py.imread("image2.jpeg")
 image = np.array(image)
 
 
 def rgb2ycbcr(imagergb):
     print ("Etape 1 - Conversion RGB - Y'CbCr..")
     imageycbcr = np.zeros_like(imagergb)
-    for row in range(len(imagergb)):
-        for col in range(len(imagergb[row])):
-            R = imagergb[row][col][0]
-            G = imagergb[row][col][1]
-            B = imagergb[row][col][2]
+    imageycbcr = imageycbcr.astype('float64')
+    for row in range(imagergb.shape[0]):
+        for col in range(imagergb.shape[1]):
+            R = imagergb[row][col][INDEX_R]
+            G = imagergb[row][col][INDEX_G]
+            B = imagergb[row][col][INDEX_B]
             Y = 0.299 * R + 0.587 * G + 0.114 * B
             Cb = 128 + 0.564 * (B - Y)
             Cr = 128 + 0.713 * (R - Y)
-            imageycbcr[row][col] = [Y,Cb,Cr]
-            # if (row % 2 == 0 and col % 2 == 0):
-            #     imageycbcr[row][col] = [Y,Cb,Cr]
-            # else:
-            #     imageycbcr[row][col] = [Y,0,0]
+            if (row % 2 == 0 and col % 2 == 0):
+                imageycbcr[row][col] = [Y,Cb,Cr]
+            else:
+                imageycbcr[row][col] = [Y,0,0]
     return imageycbcr
 
            
 def ycbcr2rgb(imageycbcr):
     imagergb = np.zeros_like(imageycbcr)
-    for row in range(len(imageycbcr)):
-        for col in range(len(imageycbcr[row])):
-            # oldi = row
-            # oldj = col
-            # Y = imageycbcr[row][col][0]
-            # if (row % 2 == 0 and col % 2 == 0):
-            #     Cb = imageycbcr[row][col][1]
-            #     Cr = imageycbcr[row][col][2]
-            # else:
-            #     if (row % 2 == 1):
-            #         oldi = row - 1
-            #     if (col % 2 == 1):
-            #         oldj = col - 1
-            #     Cb = imageycbcr[oldi][oldj][1]
-            #     Cr = imageycbcr[oldi][oldj][2]
+    for row in range(imageycbcr.shape[0]):
+        for col in range(imageycbcr.shape[1]):
+            oldi = row
+            oldj = col
             Y = imageycbcr[row][col][0]
-            Cb = imageycbcr[row][col][1]
-            Cr = imageycbcr[row][col][2]
-            R = Y + 1.403 * (Cr - 128)
-            G = Y - 0.714 * (Cr - 128) - 0.344 * (Cb - 128)
-            B = Y + 1.773 * (Cb - 128)
+            if (row % 2 == 0 and col % 2 == 0):
+                Cb = imageycbcr[row][col][INDEX_CB]
+                Cr = imageycbcr[row][col][INDEX_CR]
+            else:
+                if (row % 2 == 1):
+                    oldi = row - 1
+                if (col % 2 == 1):
+                    oldj = col - 1
+                Cb = imageycbcr[oldi][oldj][INDEX_CB]
+                Cr = imageycbcr[oldi][oldj][INDEX_CR]
+
+            Y = imageycbcr[row][col][INDEX_Y]
+
+            R = Y + (1.403 * (Cr - 128))
+            G = Y - (0.714 * (Cr - 128)) - (0.344 * (Cb - 128))
+            B = Y + (1.773 * (Cb - 128))
             imagergb[row][col] = [R,G,B]
-    return imagergb
+    return imagergb.astype('uint8')
 
-image = rgb2ycbcr(image)
-image = ycbcr2rgb(image)
+def diff(image, image2):
+    for i in range(image.shape[0]):
+        for j in range(image.shape[1]):
+            if (image[i][j] != image2[i][j]).any():
+                print( "L'index ({},{}) est différent. Image1 : {}, image2 : {}".format(i, j, image[i][j], image2[i][j]))
+image1 = rgb2ycbcr(image)
+image2 = ycbcr2rgb(image1)
 
+# diff(image, image2)
 py.imshow(image)
 py.show()
