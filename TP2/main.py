d42f1d53a44dc5f1aa67f880693e27944853ddaa
Benjamin <benjamin150594@hotmail.fr>
2019-03-01 01:11:36 -0500

    huffman fini

--- TP2/main.py
+++ TP2/main.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as py
 import numpy as np
 import scipy.fftpack as dctpack
-
+from anytree import Node, RenderTree, PreOrderIter, AsciiStyle
 INDEX_Y = 0
 INDEX_CB = 1
 INDEX_CR = 2
@@ -23,7 +23,7 @@ Quant = np.matrix('1 1 1 1 1 1 1 1;\
 
 print ("TP2 INF8770..")
 
-image = py.imread("image4.jpg")
+image = py.imread("ImageFinal4.jpg")
 image = np.array(image)
 image = image.astype('float64')
 
@@ -151,6 +151,164 @@ def inverseZigZag(image):
     return result
 
 
+def RLE(image):
+    result = []
+    for blocs in image:
+        resultBloc = []
+        for ycbcr in blocs:
+            retirer0negatif(ycbcr)
+        M = concatenateYCbCr(blocs)
+        i = 0
+        for elem in M:
+            if elem == '0.0,0.0,0.0':
+                i += 1
+            else:
+                resultBloc.append("({}, {}".format(elem, i))
+                i = 0
+        result.append(resultBloc)
+    return result
+
+def inverseRLE(image):
+    M = np.zeros_like(image) # image est un tableau, pas un np array. Il 
+    for bloc in image: 
+        i = 0
+        k = 0
+        temp = []
+        for elem in bloc:
+            Y, cb, cr, nb0 = elem.replace('(','').replace(')', '').split(',')
+            j = 0
+            while j < int(nb0):
+                M[i + k] = (0.0, 0.0, 0.0)
+                k += 1
+                j += 1
+            M[i + k] = (float(Y), float(cb), float(cr)) #ERREUR contient tuple au lieu d'array 64
+            i += 1
+    return M
+
+def retirer0negatif(elem):
+    if elem == '-0.0':
+        return '0.0'
+    else:
+        return elem
+
+def concatenateYCbCr(bloc):
+    M = []
+    for k in range(BLOCK_SIZE * BLOCK_SIZE):
+        a = retirer0negatif(str(bloc[k][0]))
+        b = retirer0negatif(str(bloc[k][1]))
+        c = retirer0negatif(str(bloc[k][2]))
+        M.append("{},{},{}".format(a,b,c))
+    return M
+
+
+def huffman(image):
+    Message = ''.join(image)
+
+    ArbreSymb =[[Message[0], Message.count(Message[0]), Node(Message[0])]] 
+    #dictionnaire obtenu à partir de l'arbre.
+    dictionnaire = [[Message[0], '']]
+    nbsymboles = 1
+
+    #Recherche des feuilles de l'arbre
+
+    for i in range(1,len(Message)):
+        if not list(filter(lambda x: x[0] == Message[i], ArbreSymb)):
+            ArbreSymb += [[Message[i], Message.count(Message[i]),Node(Message[i])]]
+            dictionnaire += [[Message[i], '']]
+            nbsymboles += 1
+            
+    longueurOriginale = np.ceil(np.log2(nbsymboles))*len(Message)
+
+    ArbreSymb = sorted(ArbreSymb, key=lambda x: x[1])
+    #print(ArbreSymb)
+
+
+
+    while len(ArbreSymb) > 1:
+        #Fusion des noeuds de poids plus faibles
+        symbfusionnes = ArbreSymb[0][0] + ArbreSymb[1][0] 
+        #Création d'un nouveau noeud
+        noeud = Node(symbfusionnes)
+        temp = [symbfusionnes, ArbreSymb[0][1] + ArbreSymb[1][1], noeud]
+        #Ajustement de l'arbre pour connecter le nouveau avec ses parents 
+        ArbreSymb[0][2].parent = noeud
+        ArbreSymb[1][2].parent = noeud
+        #Enlève les noeuds fusionnés de la liste de noeud à fusionner.
+        del ArbreSymb[0:2]
+        #Ajout du nouveau noeud à la liste et tri.
+        ArbreSymb += [temp]
+        #Pour affichage de l'arbre ou des sous-branches
+    # print('\nArbre actuel:\n\n')
+        for i in range(len(ArbreSymb)):
+            if len(ArbreSymb[i][0]) > 1:
+                #print(RenderTree(ArbreSymb[i][2], style=AsciiStyle()).by_attr())   
+                pass
+        ArbreSymb = sorted(ArbreSymb, key=lambda x: x[1])  
+        #print(ArbreSymb)
+
+
+    ArbreCodes = Node('')
+    noeud = ArbreCodes
+    #print([node.name for node in PreOrderIter(ArbreSymb[0][2])])
+    parcoursprefix = [node for node in PreOrderIter(ArbreSymb[0][2])]
+    parcoursprefix = parcoursprefix[1:len(parcoursprefix)] #ignore la racine
+
+    Prevdepth = 0 #pour suivre les mouvements en profondeur dans l'arbre
+    for node in parcoursprefix:  #Liste des noeuds 
+        if Prevdepth < node.depth: #On va plus profond dans l'arbre, on met un 0
+            temp = Node(noeud.name + '0')
+            noeud.children = [temp]
+            if node.children: #On avance le "pointeur" noeud si le noeud ajouté a des enfants.
+                noeud = temp
+        elif Prevdepth == node.depth: #Même profondeur, autre feuille, on met un 1
+            temp = Node(noeud.name + '1')
+            noeud.children = [noeud.children[0], temp]  #Ajoute le deuxième enfant
+            if node.children: #On avance le "pointeur" noeud si le noeud ajouté a des enfants.
+                noeud = temp
+        else:
+            for i in range(Prevdepth-node.depth): #On prend une autre branche, donc on met un 1
+                noeud = noeud.parent #On remontre dans l'arbre pour prendre la prochaine branche non explorée.
+            temp = Node(noeud.name + '1')
+            noeud.children = [noeud.children[0], temp]
+            if node.children:
+                noeud = temp
+            
+        Prevdepth = node.depth    
+        
+    #print('\nArbre des codes:\n\n',RenderTree(ArbreCodes, style=AsciiStyle()).by_attr())         
+    #print('\nArbre des symboles:\n\n', RenderTree(ArbreSymb[0][2], style=AsciiStyle()).by_attr())
+
+    ArbreSymbList = [node for node in PreOrderIter(ArbreSymb[0][2])]
+    ArbreCodeList = [node for node in PreOrderIter(ArbreCodes)]
+
+    for i in range(len(ArbreSymbList)):
+        if ArbreSymbList[i].is_leaf: #Génère des codes pour les feuilles seulement
+            temp = list(filter(lambda x: x[0] == ArbreSymbList[i].name, dictionnaire))
+            if temp:
+                indice = dictionnaire.index(temp[0])
+                dictionnaire[indice][1] = ArbreCodeList[i].name
+    MessageCode = []
+    longueur = 0 
+    for i in range(len(Message)):
+        substitution = list(filter(lambda x: x[0] == Message[i], dictionnaire))
+        MessageCode += [substitution[0][1]]
+        longueur += len(substitution[0][1])
+
+    return (MessageCode, dictionnaire)
+
+
+
+# https://stackoverflow.com/questions/33089660/decoding-a-huffman-code-with-a-dictionary
+def huffmanDecode (dictionary, text):
+    res = ""
+    text = ''.join(text)
+    while text:
+        for k in dictionary:
+            if text.startswith(k[1]):
+                res += k[0]
+                text = text[len(k[1]):]
+    return res
+
 print("rgb -> YCbCr..")
 image = rgb2ycbcr(image)
 
@@ -167,11 +325,40 @@ imageQuantifie = quantification(imagedct)
 print("zigzag..")
 imageZigzag = zigzag(imageQuantifie)
 
-print("InverseZigZag")
-imageInverseZigZag = inverseZigZag(imageZigzag)
+print('RLE')
+imageRLE = RLE(imageZigzag)
 
+print('Huffman')
+
+messages = []
+dictionnaires = []
+for bloc in imageRLE:
+    message, dictionnaire = huffman(bloc)
+    messages.append(message)
+    dictionnaires.append(dictionnaire)
+
+size = 0
+for message in messages: 
+    size += len(message)
+print(size)
+
+print("INVERSION")
+
+
+print('Inverse Huffman')
+blocs = []
+for i in range(0, len(messages)):
+    blocs.append(huffmanDecode(dictionnaires[i], messages[i]))
+
+print('inverse RLE')
+imageInverseRLE = inverseRLE(imageRLE)
+
+
+print("InverseZigZag")
+imageInverseZigZag = inverseZigZag(imageZigzag) # Inversion imageZigZag imageInverseRLE
+imageInverseZigZag = np.array(imageInverseZigZag)
 print('dequantification..')
-imageDequantifie = dequantification(imageQuantifie)
+imageDequantifie = dequantification(imageInverseZigZag)
 
 
 print('IDCT ...')
