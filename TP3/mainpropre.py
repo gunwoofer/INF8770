8d5449d957516e13852b114f2446456813d29ffa
Benjamin <benjamin150594@hotmail.fr>
2019-04-06 15:55:12 -0400

    fini

--- TP3/mainpropre.py
+++ TP3/mainpropre.py
@@ -3,15 +3,16 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+from scipy import signal
 
-SEUIL_CUT = 20000
-SEUIL_FADE = 20000
+SEUIL_CUT = 10000
+SEUIL_FADE = 6000
 FILENAME = "julia.avi" 
 cuts = []
 fades = []
 
 images = []
-
+fades_by_mean = []
 def rgb2gray(rgb):
     return np.dot(rgb[:,:], [0.299, 0.587, 0.114])
 
@@ -35,9 +36,15 @@ def generateMask(im):
 def generateHist(im, masks):
     hists = []
     for mask in masks:
-        for _ in range(0, 3): #rgb
-            hist = cv2.calcHist([im], [0], mask, [256], [0,256])
-            hists.append(hist)
+        #for _ in range(0, 3): #rgb
+        hist = cv2.calcHist([im], [0], mask, [256], [0,256])
+        hists.append(hist)
+    return hists
+
+def generateHistWithoutMask(im):
+    hists = []
+    hist = cv2.calcHist([im], [0], None, [256], [0,256])
+    hists.append(hist)
     return hists
 
 def regenerateCuts(cuts, trame):
@@ -49,11 +56,21 @@ def regenerateCuts(cuts, trame):
     results[:] += 1
     return results
 
+def convolution(image, matrix):
+    newImage = signal.convolve2d(rgb2gray(image).astype("uint8"), matrix)
+    for i in range(newImage.shape[0]):
+        for j in range(newImage.shape[1]):
+            if newImage[i][j] < 127:
+                newImage[i][j] = 0
+            else:
+                newImage[i][j] = 255
+    return newImage
+
 def main():
     print("TP3 INF8770..")
     cap = cv2.VideoCapture()
     cap.open(FILENAME)
- 
+
     if not cap.isOpened():
         print("Erreur pendant l'ouverture de la video %s" % FILENAME)
         return
@@ -63,42 +80,41 @@ def main():
     (rv, im) = cap.read()  
     indexTrame = 0
 
+
     # Masques
     masques = generateMask(im)
 
     histoDiffTrame = []
     liste_histo = []
+
     while rv:
         # Traitement image par image
+
         images.append(im)
+
         histQuantified = []
-        # im = rgb2gray(im).astype("uint8")
 
         # Histogrammes
         hists = generateHist(im, masques)
-
         # Concatenation
         histo = np.concatenate(hists)
-        histo_final = np.zeros((int(histo.shape[0] / 3), 1))
-        for i in range(0, int(histo.shape[0] / 3)):
-            histo_final[i] = histo[i*3:i*3+3].sum()
-        histo = histo_final
+        
         # Quantification
-        niveau = 16
+        niveau = 32 # 64 on perd des cuts + 32 on a des erreurs dans fades, mais on a tous les cut
         for i in range(0,histo.shape[0],niveau):
             histQuantified.append(histo[i:i+niveau].sum())
         liste_histo.append(histQuantified)
+         
+
         # Difference avec la trame precedante
-        if indexTrame > 0:
-            histoDiffTrame.append(abs(diffTrame(np.array(histQuantified), np.array(liste_histo[indexTrame - 1]))))
-            #histoDiffTrame.append(abs(diffTrame2(np.array(liste_histo[indexTrame]), np.array(liste_histo[indexTrame - 1]), liste_histo[indexTrame - 2])))
-
-        #histoPrec = histQuantified
         
-
+        if indexTrame > 0:
+            histoDiffTrame.append(abs(diffTrame(np.array(liste_histo[indexTrame]), np.array(liste_histo[indexTrame - 1]))))
+        #if indexTrame > 1:
+         #   histoDiffTrame.append(abs(diffTrame2(np.array(liste_histo[indexTrame]), np.array(liste_histo[indexTrame - 1]), liste_histo[indexTrame - 2])))
+        
         (rv, im) = cap.read()  
         indexTrame = indexTrame + 1
-        #print(indexTrame)
 
     histoDiffTrame = np.array(histoDiffTrame)
     cuts = np.where(np.array(histoDiffTrame) > SEUIL_FADE)
@@ -142,16 +158,15 @@ def main():
         # plt.show(block=True)
 
     for fade in real_fades:
-        # img1 = images[cut]
-        # img2 = images[cut + 1]
-        # f = plt.figure()
-        # f.add_subplot(1,2, 1)
-        # plt.imshow(img1)
-        # f.add_subplot(1,2, 2)
-        # plt.imshow(img2)
+    #     img1 = images[fade]
+    #     img2 = images[fade + 1]
+    #     f = plt.figure()
+    #     f.add_subplot(1,2, 1)
+    #     plt.imshow(img1)
+    #     f.add_subplot(1,2, 2)
+    #     plt.imshow(img2)
         print("fade : " + str(fade))
-    # for fade in fades:
-    #     print(" fades : " + str(fade))
+    #     plt.show(block=True)
 
     plt.plot(histoDiffTrame)
     plt.show()
